af567facb50403cae9740d3995fc725c42aa2ed6
suresh mandapalli <66876777+sureshunix09@users.noreply.github.com>
2020-12-02 23:19:45 -0500

    Update main.tf

--- later/main.tf
+++ later/main.tf
@@ -1,3 +1,4 @@
+
 provider "azurerm" {
 
 features {} 
@@ -17,7 +18,7 @@ resource "azurerm_virtual_network" "main" {
 }
 
 resource "azurerm_subnet" "internal" {
-  name                 = "internal"
+  name                 = "Vnet1"
   resource_group_name  = "${azurerm_resource_group.main.name}"
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
